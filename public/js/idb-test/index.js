62d895f49aee186f2aa470d68f92f6da83f07901
pvt-16 <prabha0106@gmail.com>
2018-03-31 18:55:50 +0530

    Include Indexes, cursors
    Updating index.js of idb-test to include Indexes, cursors

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,7 +1,7 @@
 import idb from 'idb';
 
 // opening database, creating object stores, checking versions, putting data
-var dbPromise = idb.open('test-db', 5, function(upgradeDb) {
+var dbPromise = idb.open('test-db', 7, function(upgradeDb) {
 
   switch ( upgradeDb.oldVersion) {
       case 0 :
@@ -9,36 +9,42 @@ var dbPromise = idb.open('test-db', 5, function(upgradeDb) {
               keyValStore.put("world", "hello");
       case 4 :
               upgradeDb.createObjectStore('people',{ keyPath : 'name'});
+      case 5:
+             var peopleStore = upgradeDb.transaction.objectStore("people");
+             peopleStore.createIndex('animal','favouriteAnimal');
+      case 6:
+             var peopleStore = upgradeDb.transaction.objectStore("people");
+             peopleStore.createIndex('age','age');
   }
 });
 
 // read "hello" in "keyval"
-dbPromise.then( function(db) {
-  var trans = db.transaction("keyval");
-  var objstore = trans.objectStore("keyval")
-  return objstore.getKey("hello");
-}).then(function (ans) {
-  console.log("The key for hello is : ", ans);
-});
+// dbPromise.then( function(db) {
+//   var trans = db.transaction("keyval");
+//   var objstore = trans.objectStore("keyval")
+//   return objstore.getKey("hello");
+// }).then(function (ans) {
+//   console.log("The key for hello is : ", ans);
+// });
 
-dbPromise.then(function(db) {
-  // TODO: in the keyval store, set
-  // "favoriteAnimal" to your favourite animal
-  // eg "cat" or "dog"
+// dbPromise.then(function(db) {
+//   // TODO: in the keyval store, set
+//   // "favoriteAnimal" to your favourite animal
+//   // eg "cat" or "dog"
 
-  var tx = db.transaction("keyval", "readwrite");
-  var dataacc = tx.objectStore("keyval");
-  dataacc.put("dog","favoriteAnimal").then(function() {
-    console.log("favoriteAnimal success")
-  }).catch(function(err) {
-    console.log("favoriteAnimal failed : ", err)
-  });
-  return tx.complete;
-}).then(function (ans) {
-  console.log("favoriteAnimal Transaction complete");
-}).catch( function(err) {
-  console.log("favoriteAnimal Transaction failed :", err)
-})
+//   var tx = db.transaction("keyval", "readwrite");
+//   var dataacc = tx.objectStore("keyval");
+//   dataacc.put("dog","favoriteAnimal").then(function() {
+//     console.log("favoriteAnimal success")
+//   }).catch(function(err) {
+//     console.log("favoriteAnimal failed : ", err)
+//   });
+//   return tx.complete;
+// }).then(function (ans) {
+//   console.log("favoriteAnimal Transaction complete");
+// }).catch( function(err) {
+//   console.log("favoriteAnimal Transaction failed :", err)
+// })
 
 // insert into the object store - >  Make a PUT out of this
 dbPromise.then( function (db) {
@@ -47,12 +53,12 @@ dbPromise.then( function (db) {
   object.put({
     name : "Samir Sheriff",
     age : 100,
-    favouriteAnimal : "Doggo"
+    favouriteAnimal : "Dog"
   })
   object.put({
     name : "Popeye",
     age : 50,
-    favouriteAnimal : "Eugene the dog"
+    favouriteAnimal : "Dog"
   })
   object.put({
     name : "Oswald",
@@ -62,7 +68,7 @@ dbPromise.then( function (db) {
   object.put({
     name : "Tam tam",
     age : 3,
-    favouriteAnimal : "pam pam"
+    favouriteAnimal : "Pam"
   })
 
   return tx.complete;
@@ -74,9 +80,62 @@ dbPromise.then( function (db) {
 dbPromise.then( function ( db) {
   var tx = db.transaction("people", "readwrite");
   var objStore = tx.objectStore("people");
-  return objStore.getAll();
+
+  // returning the data grouped by acc to the favouriteAnimal
+
+  var indexStore = objStore.index("animal");
+
+  return indexStore.getAll("Dog");
+
 }).then ( function ( allPeople) {
-  console.log ("People : ", allPeople);
+  //console.log ("People : ", allPeople);
+}).catch(function (err) {
+  console.log("Error : ", err)
+})
+
+// returning data acc to age ASC. Created 'age' index
+
+dbPromise.then( function ( db) {
+  var tx = db.transaction("people", "readwrite");
+  var objStore = tx.objectStore("people");
+
+  var indexStore = objStore.index("age");
+
+  return indexStore.getAll();
+
+}).then ( function ( allPeople) {
+  console.log ("People acc to ages : ", allPeople);
+}).catch(function (err) {
+  console.log("Error : ", err)
+})
+
+
+//Using CURSORS - returning data acc to age ASC. Created 'age' index
+
+dbPromise.then( function ( db) {
+  var tx = db.transaction("people", "readwrite");
+  var objStore = tx.objectStore("people");
+
+  var indexStore = objStore.index("age");
+
+  return indexStore.openCursor();
+
+}).then(function (cursor) {
+  if(!cursor) 
+    return;
+  else
+    return cursor.advance(2);
+}).then ( function logPerson ( cursor) {
+
+    if( !cursor) { 
+      return; 
+    }
+    else {
+      console.log ("Cursored at  : ", cursor.value.name);
+      return cursor.continue().then( logPerson )
+    }
+}).then( function() {
+  console.log("Cursoring done");
 }).catch(function (err) {
   console.log("Error : ", err)
 })
